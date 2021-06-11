1e89dba62e78c948666d514e46483ccaba2ce2fe
MonicaKian <55225156+MonicaKian@users.noreply.github.com>
2021-06-11 16:41:47 +0930

    Delete hard coded data like GetBook List and booList from the BookController

--- MVCBookList/MVCBookList/Controllers/BookController.cs
+++ MVCBookList/MVCBookList/Controllers/BookController.cs
@@ -54,7 +54,7 @@ namespace MVCBookList.Controllers
         // GET: Book/Edit/5
         public ActionResult Edit(int id)
         {
-           
+
             var book = db.Books.Single(m => m.Id == id);
             return View(book);
         }
@@ -105,79 +105,8 @@ namespace MVCBookList.Controllers
 
         }
 
-
-
-        [NonAction]
-        public List<Book> GetBookList()
-        {
-            return new List<Book>{
-      new Book{
-         Id = 1,
-         Name = "Be Kind",
-         Description = "From asking the new girl to play to standing up for someone being bullied, this moving story explores what kindness is, " +
-         "and how any act, big or small, can make a difference—or at least help a friend."
-
-      },
-
-      new Book{
-         Id = 2,
-         Name = "Panchatantra for Children",
-         Description = "The classic characters in these stories have been brought to life using simple language and attractive illustrations, " +
-         "making Panchatantra for Children a must-have, must-read book for all children."
-          },
-
-      new Book{
-         Id = 3,
-         Name = "The Journey of the Marmabill",
-         Description = "A little marmabill loses her home to a grumpy creature and thus sets off on a mission to find a new home--and " +
-         "she makes some friends along the way"
-             },
-
-      new Book{
-         Id = 4,
-         Name = "I Am the Dog",
-         Description = "The question is: Who has the better deal? Story maestro Daniel Pinkwater collaborates once again with the gifted " +
-         "illustrator Jack E. Davis for a boy-and - dog escapade that's sure to set tails a-wagging"
-          },
-         };
-        }
-
-
-        public static List<Book> booList = new List<Book>{
-       new Book{
-         Id = 1,
-         Name = "Be Kind",
-         Description = "From asking the new girl to play to standing up for someone being bullied, this moving story explores what kindness is, " +
-         "and how any act, big or small, can make a difference—or at least help a friend."
-
-      },
-
-      new Book{
-         Id = 2,
-         Name = "Panchatantra for Children",
-         Description = "The classic characters in these stories have been brought to life using simple language and attractive illustrations, " +
-         "making Panchatantra for Children a must-have, must-read book for all children."
-          },
-
-      new Book{
-         Id = 3,
-         Name = "The Journey of the Marmabill",
-         Description = "A little marmabill loses her home to a grumpy creature and thus sets off on a mission to find a new home--and " +
-         "she makes some friends along the way"
-             },
-
-      new Book{
-         Id = 4,
-         Name = "I Am the Dog",
-         Description = "The question is: Who has the better deal? Story maestro Daniel Pinkwater collaborates once again with the gifted " +
-         "illustrator Jack E. Davis for a boy-and - dog escapade that's sure to set tails a-wagging"
-          },
-
-
-    };
-
+       
     }
-
     public class BoDBContext : DbContext
     {
         public BoDBContext()
