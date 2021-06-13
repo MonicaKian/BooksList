67533861edc513f2b5f0d9c9a3cb763703668206
MonicaKian <parmy050@mymail.unisa.edu.au>
2021-06-13 21:39:58 +0930

    Update HomeController Index Test

--- MVCBookList/MVCBookList.Tests/Controllers/HomeControllerTest.cs
+++ MVCBookList/MVCBookList.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MVCBookList;
@@ -19,9 +22,7 @@ namespace MVCBookList.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.",
-            result.ViewBag.Message);
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
